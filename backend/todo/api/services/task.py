880045300164a31e5997fa66f6fcdb7d629aeb2d
hershamin <h.amin@utexas.edu>
2018-02-20 00:27:14 -0600

    Added task service and unit tests in backend

--- backend/todo/api/services/task.py
+++ backend/todo/api/services/task.py
@@ -1,5 +1,6 @@
-from ..models import Task
+from ..models import Task, User
 from ..misc.utils import AESCipher
+from ..misc.choices import to_internal, to_external, TaskStatus, TaskPriority
 
 
 class TaskService():
@@ -9,20 +10,68 @@ class TaskService():
 
     def create_task(self, token, title, description, priority):
         """Return task_id if created or nothing"""
-        return
+        try:
+            encToken = self.aes.encrypt(token)
+            user = User.objects.get(current_session=encToken)
+        except Exception:
+            return
+        task = Task(title=title, description=description, user=user,
+                    task_priority=to_internal(TaskPriority, priority))
+        task.save()
+        return task.task_id
 
     def complete_task(self, token, task_id):
         """Return True if completed or False"""
-        return False
+        try:
+            encToken = self.aes.encrypt(token)
+            user = User.objects.get(current_session=encToken)
+            task = Task.objects.get(task_id=task_id, user=user)
+        except Exception:
+            return False
+        task.completed = True
+        task.save()
+        return True
 
     def delete_task(self, token, task_id):
         """Return True if deleted or False"""
-        return False
-
-    def get_tasks(self, token):
-        """Return array of tasks"""
-        return []
+        try:
+            encToken = self.aes.encrypt(token)
+            user = User.objects.get(current_session=encToken)
+            task = Task.objects.get(task_id=task_id, user=user)
+        except Exception:
+            return False
+        task.delete()
+        return True
 
     def set_task_priority(self, token, task_id, priority):
         """Return True if set priority or False"""
-        return False
+        try:
+            encToken = self.aes.encrypt(token)
+            user = User.objects.get(current_session=encToken)
+            task = Task.objects.get(task_id=task_id, user=user)
+        except Exception:
+            return False
+        task.task_priority = to_internal(TaskPriority, priority)
+        task.save()
+        return True
+
+    def get_tasks(self, token):
+        """Return array of tasks"""
+        try:
+            encToken = self.aes.encrypt(token)
+            user = User.objects.get(current_session=encToken)
+        except Exception:
+            return []
+        tasks = Task.objects.filter(user=user).order_by('-created_at')
+        resp = []
+        for task in tasks:
+            temp = {
+                'title': task.title,
+                'description': task.description,
+                'priority': to_external(TaskPriority, task.task_priority),
+                'completed': task.completed,
+                'status': to_external(TaskStatus, task.task_status),
+                'id': task.task_id
+            }
+            resp.append(temp)
+        return resp
--- backend/todo/api/tests/test_task_service.py
+++ backend/todo/api/tests/test_task_service.py
@@ -1,11 +1,115 @@
 from django.test import TestCase
-from ..models import Task
+from ..services import TaskService, UserService
 
 
 class TaskServiceTestCase(TestCase):
 
     def setUp(self):
-        return
+        self.taskService = TaskService()
+        self.userService = UserService()
+        self.user = self.userService.create_user('first', 'last', 'info@email.com', 'password')
+        self.token = self.userService.login_user(self.user.get('email'), 'password').get('login_token')
 
     def test_create_task(self):
-        return
+        taskId = self.taskService.create_task(self.token, 'title', 'desc', 'Low')
+        self.assertIsNotNone(taskId)
+        tasks = self.taskService.get_tasks(self.token)
+        self.assertEqual(len(tasks), 1)
+        task = tasks.__getitem__(0)
+        self.assertEqual(task.get('title'), 'title')
+        self.assertEqual(task.get('description'), 'desc')
+        self.assertEqual(task.get('priority'), 'Low')
+        self.assertEqual(task.get('completed'), False)
+        self.assertEqual(task.get('status'), 'Open')
+        self.assertIsNotNone(task.get('id'))
+
+    def test_create_task_unknown_user(self):
+        taskId = self.taskService.create_task('fake token', 'title', 'desc', 'Low')
+        self.assertIsNone(taskId)
+        tasks = self.taskService.get_tasks(self.token)
+        self.assertEqual(len(tasks), 0)
+
+    def test_complete_task(self):
+        taskId = self.taskService.create_task(self.token, 'title', 'desc', 'Low')
+        status = self.taskService.complete_task(self.token, taskId)
+        self.assertTrue(status)
+        tasks = self.taskService.get_tasks(self.token)
+        self.assertEqual(len(tasks), 1)
+        task = tasks.__getitem__(0)
+        self.assertEqual(task.get('id'), taskId)
+        self.assertEqual(task.get('completed'), True)
+
+    def test_complete_task_unknown_user(self):
+        taskId = self.taskService.create_task(self.token, 'title', 'desc', 'Medium')
+        status = self.taskService.complete_task('fake token', taskId)
+        self.assertFalse(status)
+        tasks = self.taskService.get_tasks(self.token)
+        self.assertEqual(len(tasks), 1)
+        task = tasks.__getitem__(0)
+        self.assertEqual(task.get('id'), taskId)
+        self.assertEqual(task.get('completed'), False)
+
+    def test_complete_task_wrong_id(self):
+        taskId = self.taskService.create_task(self.token, 'title', 'desc', 'High')
+        status = self.taskService.complete_task(self.token, taskId + 2)
+        self.assertFalse(status)
+        tasks = self.taskService.get_tasks(self.token)
+        self.assertEqual(len(tasks), 1)
+        task = tasks.__getitem__(0)
+        self.assertEqual(task.get('id'), taskId)
+        self.assertEqual(task.get('completed'), False)
+
+    def test_delete_task(self):
+        taskId = self.taskService.create_task(self.token, 'title', 'desc', 'High')
+        status = self.taskService.delete_task(self.token, taskId)
+        self.assertTrue(status)
+        tasks = self.taskService.get_tasks(self.token)
+        self.assertEqual(len(tasks), 0)
+
+    def test_delete_task_unknown_user(self):
+        taskId = self.taskService.create_task(self.token, 'title', 'desc', 'High')
+        status = self.taskService.delete_task('fake token', taskId)
+        self.assertFalse(status)
+        tasks = self.taskService.get_tasks(self.token)
+        self.assertEqual(len(tasks), 1)
+        task = tasks.__getitem__(0)
+        self.assertEqual(task.get('id'), taskId)
+
+    def test_delete_task_wrong_id(self):
+        taskId = self.taskService.create_task(self.token, 'title', 'desc', 'High')
+        status = self.taskService.delete_task(self.token, taskId + 2)
+        self.assertFalse(status)
+        tasks = self.taskService.get_tasks(self.token)
+        self.assertEqual(len(tasks), 1)
+        task = tasks.__getitem__(0)
+        self.assertEqual(task.get('id'), taskId)
+
+    def test_set_priority(self):
+        taskId = self.taskService.create_task(self.token, 'title', 'desc', 'High')
+        status = self.taskService.set_task_priority(self.token, taskId, 'Medium')
+        self.assertTrue(status)
+        tasks = self.taskService.get_tasks(self.token)
+        self.assertEqual(len(tasks), 1)
+        task = tasks.__getitem__(0)
+        self.assertEqual(task.get('id'), taskId)
+        self.assertEqual(task.get('priority'), 'Medium')
+
+    def test_set_priority_unknown_user(self):
+        taskId = self.taskService.create_task(self.token, 'title', 'desc', 'High')
+        status = self.taskService.set_task_priority('fake token', taskId, 'Medium')
+        self.assertFalse(status)
+        tasks = self.taskService.get_tasks(self.token)
+        self.assertEqual(len(tasks), 1)
+        task = tasks.__getitem__(0)
+        self.assertEqual(task.get('id'), taskId)
+        self.assertEqual(task.get('priority'), 'High')
+
+    def test_set_priority_wrong_id(self):
+        taskId = self.taskService.create_task(self.token, 'title', 'desc', 'High')
+        status = self.taskService.set_task_priority(self.token, taskId + 2, 'Medium')
+        self.assertFalse(status)
+        tasks = self.taskService.get_tasks(self.token)
+        self.assertEqual(len(tasks), 1)
+        task = tasks.__getitem__(0)
+        self.assertEqual(task.get('id'), taskId)
+        self.assertEqual(task.get('priority'), 'High')
